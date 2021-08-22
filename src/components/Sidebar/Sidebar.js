ff5d5611e751d75b54a5d25ac78d1a82f4f48fff
a.k.a.a.h.l <a.k.afiq@live.com>
2021-08-22 17:10:53 +0800

    Added more SidebarOption components

--- src/components/Sidebar/Sidebar.js
+++ src/components/Sidebar/Sidebar.js
@@ -4,6 +4,12 @@ import "./Sidebar.css";
 import AddIcon from "@material-ui/icons/Add";
 import SidebarOption from "./SidebarOption/SidebarOption";
 import InboxIcon from "@material-ui/icons/Inbox";
+import StarIcon from "@material-ui/icons/Star";
+import AccessTimeIcon from "@material-ui/icons/AccessTime";
+import LabelImportantIcon from "@material-ui/icons/LabelImportant";
+import NearMeIcon from "@material-ui/icons/NearMe";
+import NoteIcon from "@material-ui/icons/Note";
+import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 
 function Sidebar() {
   return (
@@ -15,7 +21,18 @@ function Sidebar() {
         Compose
       </Button>
 
-      <SidebarOption Icon={InboxIcon} title="Inbox" number={54} />
+      <SidebarOption
+        Icon={InboxIcon}
+        title="Inbox"
+        number={54}
+        selected={true}
+      />
+      <SidebarOption Icon={StarIcon} title="Starred" number={21} />
+      <SidebarOption Icon={AccessTimeIcon} title="Snoozed" number={54} />
+      <SidebarOption Icon={LabelImportantIcon} title="Important" number={54} />
+      <SidebarOption Icon={NearMeIcon} title="Sent" number={54} />
+      <SidebarOption Icon={NoteIcon} title="Drafts" number={54} />
+      <SidebarOption Icon={ExpandMoreIcon} title="More" number={54} />
     </div>
   );
 }
